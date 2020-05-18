f8389a350f940a6107515685ec5f2e5920ec33e0
naveenpandurangi <naveenpandurangi@yahoo.com>
2020-05-18 20:27:36 +0530

    ADD: ESLint pretier tailwind styled components pwa config for next js stater

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import App from 'next/app'
+import '../styles.css'
+
+export default class TailwindApp extends App {
+  render() {
+    const { Component, pageProps } = this.props
+    return <Component {...pageProps} />
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export default () => (
+  <div>
+    <Title>UAM</Title>
+  </div>
+)
+
+const Title = styled.h1.attrs({
+  className: 'm-12 text-blue-700 text-lg font-bold',
+})``
